e53c5afe09f2df8586cf3fb8cc96191d0ff89b9f
Tran Hoai Nam (FHM.EMU) <NamTH22@fsoft.com.vn>
2019-03-21 16:34:54 +0700

    amend

--- app-dkmh/src/app/components/navigation/navigation.component.ts
+++ app-dkmh/src/app/components/navigation/navigation.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { items, links } from '../../contants/menus.contants';
+
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent implements OnInit {
+  items: string[] = items;
+  links: string[] = links;
 
   constructor() { }
 
