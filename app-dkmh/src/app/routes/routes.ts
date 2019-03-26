918324367fc11b2d84014914d5f3b4bdb6607abe
Tran Hoai Nam (FHM.EMU) <NamTH22@fsoft.com.vn>
2019-03-26 09:04:05 +0700

    update routes

--- app-dkmh/src/app/routes/routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomePageComponent } from '../pages/home-page/home-page.component';
-
-const routesConfig: Routes = [
-    { path: 'Home', component: HomePageComponent }
-]
-
-export default routesConfig;
